ba0f8fe99e7ad8d1ae76c5b13bbe1e2615e4acbb
Daniel Edgar <aknot242@gmail.com>
2021-04-06 09:55:55 -0700

    update lab title

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 # ------------
 #
 # REQUIRED: Your class/lab name
-classname = "NGINX Controller"
+classname = "NGINX Controller - ADC (+ Security)"
 
 # OPTIONAL: The URL to the GitHub Repository for this class
 github_repo = "https://github.com/nginxinc/nginx-controller-lab"
